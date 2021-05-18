8d9799073dab539bc5de680f67e043970d180977
Engels <engelmmanuel@gmail.com>
2021-05-18 03:17:05 -0700

    Removed unnecessary imports from dashboard activity

--- app/src/main/java/com/engelsimmanuel/truckchecklist/screens/DashboardActivity.kt
+++ app/src/main/java/com/engelsimmanuel/truckchecklist/screens/DashboardActivity.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
